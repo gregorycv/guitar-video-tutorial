e9e234a1595046c2f00206d69a4098469977bd58
Hryhorii Havryliuk <gregorygavrilyuk@gmail.com>
2024-03-14 22:19:44 +0100

    add tailwind and nextUi libs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Providers } from "./providers";
+
 import 'normalize.css'
+import '../app/globals.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,11 +18,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className='dark'>
       <body className={inter.className}>
-        <header>header</header>
-        {children}
-        <footer>footer</footer>  
+        <Providers>
+          <header className="text-3xl font-bold underline">header</header>
+          {children}
+          <footer>footer</footer>  
+        </Providers>
       </body>
     </html>
   );
